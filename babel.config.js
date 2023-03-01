a891a68c837751de308f0b263bfe75b3324efb70
Daouadji Aymen <daouadji506@gmail.com>
2023-03-01 18:38:33 +0100

    ✨ configure the project to work with typescript

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,35 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
-   
+    presets: ['babel-preset-expo'],
+    plugins: [
+      ['@babel/plugin-transform-flow-strip-types'],
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      ['@babel/plugin-proposal-class-properties', { loose: true }],
+      [
+        'module-resolver',
+        {
+          alias: {
+            '*': ['node_modules/*'],
+            '@components': './Components',
+            '@buttons': './Components/basic/buttons',
+            // "@containers/*": ["src/renderer/ui/containers/*"],
+            '@Screens': './Components/Screens',
+            '@assets': './assets/*',
+            // "@libs/*": ["src/renderer/libs/*"],
+            '@config': './Components/config',
+            // "@helpers/*": ["src/renderer/utils/helpers/*"],
+            // "@models/*": ["src/renderer/models/*"],
+            toSvg: './assets/svg',
+            toPng: './assets/pictures',
+            '@api': './API',
+            '@redux': './services/redux',
+            '@colors': './Components/config/Colors.ts',
+            '@stores': './services/zustand',
+          },
+          extensions: ['.ts', '.tsx'],
+        },
+      ],
+    ],
   };
 };
