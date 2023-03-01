73a072cc204530e40d876810f9ad8594a869739f
Daouadji Aymen <daouadji506@gmail.com>
2023-03-01 18:37:13 +0100

    ✨ configure EsLint

---
+++ .eslintrc.js
@@ -0,0 +1,81 @@
+module.exports = {
+    root: true,
+
+      extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+      settings: {
+    react: {
+      version: 'detect', // React version. "detect" automatically picks the version you have installed.
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'react-hooks',
+    'unused-imports',
+  ],
+  globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
+
+
+    'react/prop-types': 0,
+    'no-console': 'warn',
+    'linebreak-style': 'off',
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'react-hooks/rules-of-hooks': 2,
+    'react-hooks/exhaustive-deps': 1,
+    'no-empty-pattern': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'unused-imports/no-unused-imports': 'warn',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'typeProperty',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        leadingUnderscore: 'allow',
+        trailingUnderscore: 'allow',
+      },
+    ],
+  },
+  ignorePatterns: ['*.config.js', '.eslintrc.js'],
+};
