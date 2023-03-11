0ce2c9e114f1be05b27513d6cc2f5560ef6e7363
Daouadji Aymen <daouadji506@gmail.com>
2023-03-11 21:58:28 +0100

    ✨ create AuthNavigator

--- navigation/auth_navigator/AuthNavigator.tsx
+++ navigation/auth_navigator/AuthNavigator.tsx
@@ -1,7 +1,38 @@
-import { View } from 'react-native';
-import styles from './style/index';
+import { useState } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from '@components/Screens/welcome_screen';
+
+const AuthStack = createNativeStackNavigator();
 
 interface AuthNavigatorProps {}
 export default function AuthNavigator({}: AuthNavigatorProps) {
-  return <View style={styles.AuthNavigator}></View>;
+  const [loading, setLoading] = useState(true);
+
+  // useEffect(() => {
+  //   const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
+  //     if (userAuth) {
+  //       const userRef = await getUserInfo(userAuth);
+  //       userRef.onSnapshot((snapShot) => {
+  //         console.log('id : ', snapShot.id);
+  //         set_user({
+  //           ...snapShot.data(),
+  //           id: snapShot.id,
+  //         });
+  //         setLoading(false);
+  //       });
+  //     } else {
+  //       set_user(null);
+  //       setLoading(false);
+  //     }
+  //   });
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [set_user]);
+
+  return (
+    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+      <AuthStack.Screen name="Home" component={WelcomeScreen} />
+    </AuthStack.Navigator>
+  );
 }
