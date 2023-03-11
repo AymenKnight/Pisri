5fc7a8989bc7842f12ea176c54b9ca8097db7fbe
Daouadji Aymen <daouadji506@gmail.com>
2023-03-11 21:59:34 +0100

    ✅ update WelcomeScreen comp

--- UI/Components/Screens/welcome_screen/WelcomeScreen.tsx
+++ UI/Components/Screens/welcome_screen/WelcomeScreen.tsx
@@ -4,47 +4,51 @@ import AppButton from '@components/basic/buttons/text_button/TextButton';
 import Colors from '@components/config/Colors';
 import sign_background from '@toPng/sign_background.png';
 import WelcomeLogo from '@toSvg/welcome_logo.svg';
+import { useAuthStore } from '@stores/authStore';
 
 interface WelcomeScreenProps {}
 export default function WelcomeScreen({}: WelcomeScreenProps) {
+  const {
+    signInVisible,
+    signUpVisible,
+    changeSignInVisible,
+    changeSignUpVisible,
+  } = useAuthStore();
   return (
-    <View style={styles.WelcomeScreen}>
-      <ImageBackground source={sign_background} style={styles.ImageBackground}>
-        <WelcomeLogo style={styles.logo} />
-        <View style={styles.controlsContainer}>
-          <AppButton
-            text="Sign Up "
-            backgroundColor={Colors.primary}
-
-            // onPress={() => setSignUpVisible(true)}
-          />
-          <AppButton
-            text="Sign In "
-            backgroundColor={Colors.secondary}
-
-            // onPress={() => setSignInVisible(true)}
-          />
-        </View>
-      </ImageBackground>
-    </View>
-  );
-  {
-    /* <SignVisibleContext.Provider
-          value={{
-            SignInVisible: SignInVisible,
-            setSignInVisible: setSignInVisible,
-            SignUpVisible: SignUpVisible,
-            setSignUpVisible: setSignUpVisible,
-          }}
+    <>
+      <View style={styles.WelcomeScreen}>
+        <ImageBackground
+          source={sign_background}
+          style={styles.ImageBackground}
         >
-          <SignInForm
-            visible={SignInVisible}
-            setVisible={() => setSignInVisible(false)}
-          />
-          <SignUpForm
-            visible={SignUpVisible}
-            setVisible={() => setSignUpVisible(false)}
-          />
-        </SignVisibleContext.Provider> */
-  }
+          <WelcomeLogo style={styles.logo} />
+          <View style={styles.controlsContainer}>
+            <AppButton
+              text="Sign Up "
+              backgroundColor={Colors.primary}
+              onPress={() => {
+                changeSignUpVisible();
+              }}
+            />
+            <AppButton
+              text="Sign In "
+              backgroundColor={Colors.secondary}
+              onPress={() => {
+                changeSignInVisible();
+              }}
+            />
+          </View>
+        </ImageBackground>
+      </View>
+
+      {/* <SignInForm
+        visible={SignInVisible}
+        setVisible={() => setSignInVisible(false)}
+      />
+      <SignUpForm
+        visible={SignUpVisible}
+        setVisible={() => setSignUpVisible(false)}
+      /> */}
+    </>
+  );
 }
