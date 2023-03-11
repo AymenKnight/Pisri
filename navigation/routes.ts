9fc550397916963d9ffd05d35263a3d9fb3eacb2
Daouadji Aymen <daouadji506@gmail.com>
2023-03-11 21:57:16 +0100

    ➕ add routes path alias

---
+++ navigation/routes.ts
@@ -0,0 +1,15 @@
+export default Object.freeze({
+  HOME_Stack: 'home_Stack',
+  HOME_Stack2: 'home_Stack2',
+  STORE: 'Store',
+  MY_BAG: 'my_bag',
+  DELIVERY: 'delivery',
+  WELCOME: 'Welcome',
+  APP: 'App',
+  ACTIVITY: 'Activity',
+  CHECK: 'check',
+  DELIVERY_FINISHED: 'Delivery Finished',
+  ORDER_ON_WAY: 'Order_On_Way',
+  CONGRATULATIONS: 'Congratulations',
+});
+//TODO Reformat the routes
