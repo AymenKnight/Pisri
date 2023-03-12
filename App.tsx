727e438d3fb77800e7962580cfb337557e5cc1f3
Daouadji Aymen <daouadji506@gmail.com>
2023-03-12 18:43:34 +0100

    ✨ use Overlay in the root of the app and clean up

--- App.tsx
+++ App.tsx
@@ -1,18 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from '@navigation/auth_navigator';
-
-// const styles = StyleSheet.create({
-//   container: {
-//     display: 'flex',
-//     flexDirection: 'column',
-//     gap: 10,
-//     flex: 1,
-//     backgroundColor: '#fff',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     padding: 20,
-//   },
-// });
+import Overlay from '@containers/modals/overlay';
 
 export default function App() {
   return (
@@ -20,7 +8,7 @@ export default function App() {
     // theme={{
     //   dark: false,
     //   colors: {
-    //     background: Colors.white,
+    //     background: signInVisible ? Colors.black : Colors.white,
     //     border: Colors.borders,
     //     text: Colors.black,
     //     primary: Colors.primary,
@@ -30,24 +18,8 @@ export default function App() {
     // }}
     >
       <AuthNavigator />
+      <Overlay />
     </NavigationContainer>
-
-    // <View style={styles.container}>
-    //   <AppTextInput
-    //     label="Name"
-    //     itemsDirection="row"
-    //     expoIcon={{
-    //       expoIconLibrary: 'MaterialCommunityIcons',
-    //       expoIconName: 'magnify',
-    //       size: 25,
-    //     }}
-    //   />
-    //   <PasswordInput />
-    //   <NumberInputReducer />
-    //   <UnitValueReducer value={`x${2}`} />
-
-    //   <StatusBar style="auto" />
-    // </View>
   );
 }
 // export { default } from './storybook';
