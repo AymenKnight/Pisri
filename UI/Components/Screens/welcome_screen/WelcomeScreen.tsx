d9aa7cbfc5b9e2d308da3bab098331a5a1a2f443
Daouadji Aymen <daouadji506@gmail.com>
2023-03-12 18:42:33 +0100

    ✨ test Overlay

--- UI/Components/Screens/welcome_screen/WelcomeScreen.tsx
+++ UI/Components/Screens/welcome_screen/WelcomeScreen.tsx
@@ -4,51 +4,49 @@ import AppButton from '@components/basic/buttons/text_button/TextButton';
 import Colors from '@components/config/Colors';
 import sign_background from '@toPng/sign_background.png';
 import WelcomeLogo from '@toSvg/welcome_logo.svg';
-import { useAuthStore } from '@stores/authStore';
+import { useOverlayStore } from '@stores/overlayStore';
+import PrimaryButton from '@components/basic/buttons/primary_button';
 
 interface WelcomeScreenProps {}
 export default function WelcomeScreen({}: WelcomeScreenProps) {
-  const {
-    signInVisible,
-    signUpVisible,
-    changeSignInVisible,
-    changeSignUpVisible,
-  } = useAuthStore();
+  const { modal, close } = useOverlayStore();
   return (
-    <>
-      <View style={styles.WelcomeScreen}>
-        <ImageBackground
-          source={sign_background}
-          style={styles.ImageBackground}
-        >
-          <WelcomeLogo style={styles.logo} />
-          <View style={styles.controlsContainer}>
-            <AppButton
-              text="Sign Up "
-              backgroundColor={Colors.primary}
-              onPress={() => {
-                changeSignUpVisible();
-              }}
-            />
-            <AppButton
-              text="Sign In "
-              backgroundColor={Colors.secondary}
-              onPress={() => {
-                changeSignInVisible();
-              }}
-            />
-          </View>
-        </ImageBackground>
-      </View>
-
-      {/* <SignInForm
-        visible={SignInVisible}
-        setVisible={() => setSignInVisible(false)}
-      />
-      <SignUpForm
-        visible={SignUpVisible}
-        setVisible={() => setSignUpVisible(false)}
-      /> */}
-    </>
+    <View style={styles.WelcomeScreen}>
+      <ImageBackground source={sign_background} style={styles.ImageBackground}>
+        <WelcomeLogo style={styles.logo} />
+        <View style={styles.controlsContainer}>
+          <AppButton
+            text="Sign Up "
+            backgroundColor={Colors.primary}
+            onPress={() => {
+              close();
+            }}
+          />
+          <AppButton
+            text="Sign In "
+            backgroundColor={Colors.secondary}
+            onPress={() => {
+              modal(
+                <View>
+                  <AppButton
+                    text="Sign Up "
+                    backgroundColor={Colors.primary}
+                    onPress={() => {
+                      close();
+                    }}
+                  />
+                  <AppButton
+                    text="Sign In "
+                    backgroundColor={Colors.secondary}
+                  />
+                  <PrimaryButton text="test" />
+                  <PrimaryButton text="test" />
+                </View>,
+              ).open();
+            }}
+          />
+        </View>
+      </ImageBackground>
+    </View>
   );
 }
