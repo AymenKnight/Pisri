4720b31ede8140b9962898d543663388e3089881
Daouadji Aymen <daouadji506@gmail.com>
2023-03-14 00:28:19 +0100

    🚑 load fonts in the root

--- navigation/auth_navigator/AuthNavigator.tsx
+++ navigation/auth_navigator/AuthNavigator.tsx
@@ -1,12 +1,45 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import WelcomeScreen from '@screens/welcome_screen';
+import { View } from 'react-native';
+import {
+  useFonts,
+  PublicSans_800ExtraBold,
+  PublicSans_300Light,
+  PublicSans_200ExtraLight,
+  PublicSans_400Regular,
+  PublicSans_500Medium,
+  PublicSans_600SemiBold,
+  PublicSans_700Bold,
+  PublicSans_900Black,
+} from '@expo-google-fonts/public-sans';
+import * as SplashScreen from 'expo-splash-screen';
 
 const AuthStack = createNativeStackNavigator();
+SplashScreen.preventAutoHideAsync();
 
 interface AuthNavigatorProps {}
 export default function AuthNavigator({}: AuthNavigatorProps) {
   const [loading, setLoading] = useState(true);
+  const [fontsLoaded] = useFonts({
+    PublicSans_800ExtraBold,
+    PublicSans_300Light,
+    PublicSans_200ExtraLight,
+    PublicSans_400Regular,
+    PublicSans_500Medium,
+    PublicSans_600SemiBold,
+    PublicSans_700Bold,
+    PublicSans_900Black,
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   // useEffect(() => {
   //   const unsubscribe = auth.onAuthStateChanged(async (userAuth) => {
@@ -31,8 +64,10 @@ export default function AuthNavigator({}: AuthNavigatorProps) {
   // }, [set_user]);
 
   return (
-    <AuthStack.Navigator screenOptions={{ headerShown: false }}>
-      <AuthStack.Screen name="Home" component={WelcomeScreen} />
-    </AuthStack.Navigator>
+    <View onLayout={onLayoutRootView} style={{ flex: 1 }}>
+      <AuthStack.Navigator screenOptions={{ headerShown: false }}>
+        <AuthStack.Screen name="Home" component={WelcomeScreen} />
+      </AuthStack.Navigator>
+    </View>
   );
 }
