b58dff17a23f1db25600f7f9d380e4a678d2b8df
Daouadji Aymen <daouadji506@gmail.com>
2023-03-17 15:45:56 +0100

    ✨ re-implemnte firebase authentication functions

--- API/firebase/firebase.utils.ts
+++ API/firebase/firebase.utils.ts
@@ -1,46 +1,55 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { collection, doc, getFirestore, setDoc } from 'firebase/firestore';
+import {
+  initializeAuth,
+  getReactNativePersistence,
+} from 'firebase/auth/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyDD_71rSezjEGs9_4RaXPjkPINDp5qHSsc',
+  authDomain: 'pisri-f783a.firebaseapp.com',
+  projectId: 'pisri-f783a',
+  storageBucket: 'pisri-f783a.appspot.com',
+  messagingSenderId: '995205827781',
+  appId: '1:995205827781:web:47aa246bf2a3a8ad8a74ac',
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app();
-}
+const firebaseApp =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
+const auth = initializeAuth(firebaseApp, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+const db = getFirestore();
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
-
-export const addUserOnSignUp = (
-  userAuth: firebase.auth.UserCredential | null,
+export const addUserOnSignUp = async (
   displayName: string,
-): firebase.firestore.DocumentReference | null => {
-  if (!userAuth) return null;
+  email: string,
+  password: string,
+) => {
+  try {
+    // Create user account in Firebase Authentication
+    const { user } = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password,
+    );
 
-  const email = userAuth.user?.email;
+    // Set the display name for the user
+    await updateProfile(user, { displayName });
 
-  return firestore.collection('users').add({
-    email,
-    displayName,
-  });
-};
+    // Store user data in Firestore
+    const userDocRef = doc(collection(db, 'users'), user.uid);
+    await setDoc(userDocRef, {
+      displayName,
+      email,
+    });
 
-export const getUserInfo = async (
-  userAuth: firebase.auth.UserCredential,
-): Promise<
-  firebase.firestore.DocumentReference<firebase.firestore.DocumentData>
-> => {
-  const userRef = await firestore.collection('users').doc(userAuth.user?.uid);
-  return userRef;
+    console.log('User created successfully!');
+  } catch (error) {
+    console.error('Error creating user: ', error);
+  }
 };
 
 // export const addCollectionToFirebase = async (
@@ -56,21 +65,24 @@ export const getUserInfo = async (
 //   return await batch.commit();
 // };
 
-export const convertCollectionsSnapshotToMap = (
-  snapshot: firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>,
-): Record<string, any> => {
-  const transformatedCollections = snapshot.docs.map((doc) => {
-    const { name, products } = doc.data();
-    return {
-      id: doc.id,
-      name,
-      products,
-    };
-  });
-  return transformatedCollections.reduce((acc, collection) => {
-    acc[collection.name.toLowerCase()] = collection;
-    return acc;
-  }, {});
-};
-
-export default firebase;
+// export const convertCollectionsSnapshotToMap = (
+//   snapshot: firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>,
+// ): Record<string, any> => {
+//   const transformatedCollections = snapshot.docs.map((doc) => {
+//     const { name, products } = doc.data();
+//     return {
+//       id: doc.id,
+//       name,
+//       products,
+//     };
+//   });
+//   return transformatedCollections.reduce(
+//     (acc: Record<string, any>, collection) => {
+//       acc[collection.name.toLowerCase()] = collection;
+//       return acc;
+//     },
+//     {},
+//   );
+// };
+export { auth, db };
+export default firebaseApp;
