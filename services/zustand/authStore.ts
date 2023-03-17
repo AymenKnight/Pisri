3a3fa7a45215b8c3514efdb0abf690473ef79334
Daouadji Aymen <daouadji506@gmail.com>
2023-03-17 15:47:43 +0100

    ✅ update useAuthStore

--- services/zustand/authStore.ts
+++ services/zustand/authStore.ts
@@ -1,19 +1,29 @@
 import { create } from 'zustand';
 
+interface User {
+  id: string;
+  displayName: string;
+  email: string;
+}
 interface AuthState {
-  user: firebase.User | null;
+  user: User | null;
   isLoading: boolean;
+  isFetching: boolean;
   error: string | null;
-  setUser: (user: firebase.User | null) => void;
+  setUser: (user: User | null) => void;
   setIsLoading: (isLoading: boolean) => void;
   setError: (error: string | null) => void;
+  setIsFetching: (isFetching: boolean) => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   isLoading: false,
+  isFetching: false,
   error: null,
-  setUser: (user: firebase.User | null) => set({ user }),
+  setUser: (user: User | null) =>
+    set({ user, isLoading: false, error: null, isFetching: false }),
   setIsLoading: (isLoading: boolean) => set({ isLoading }),
   setError: (error: string | null) => set({ error }),
+  setIsFetching: (isFetching: boolean) => set({ isFetching }),
 }));
