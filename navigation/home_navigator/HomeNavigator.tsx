6d19c48d4ed0654f0094d931598dc5fd5d5e0efd
Daouadji Aymen <daouadji506@gmail.com>
2023-04-08 03:43:29 +0100

    ✨ use QueryClientProvider (test)

--- navigation/home_navigator/HomeNavigator.tsx
+++ navigation/home_navigator/HomeNavigator.tsx
@@ -8,10 +8,12 @@ import TopNavigation from '@components/top_navigation';
 import StoreScreen from '@screens/store_screen';
 import BottomTabBar from '@components/bottom_tab_bar';
 import AppText from '@components/basic/app_text';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const BottomTab = createBottomTabNavigator();
 interface HomeNavigatorProps {}
 export default function HomeNavigator({}: HomeNavigatorProps) {
+  const queryClient = new QueryClient();
   const CheckView = () => (
     <View>
       <AppText text={'Check please'} />
@@ -19,14 +21,16 @@ export default function HomeNavigator({}: HomeNavigatorProps) {
   );
 
   return (
-    <BottomTab.Navigator
-      screenOptions={{
-        header: (props) => <TopNavigation />,
-      }}
-      tabBar={(props) => <BottomTabBar props={props} />}
-    >
-      <BottomTab.Screen name={routes.STORE} component={StoreScreen} />
-      <BottomTab.Screen name={routes.CHECK} component={CheckView} />
-    </BottomTab.Navigator>
+    <QueryClientProvider client={queryClient}>
+      <BottomTab.Navigator
+        screenOptions={{
+          header: (props) => <TopNavigation />,
+        }}
+        tabBar={(props) => <BottomTabBar props={props} />}
+      >
+        <BottomTab.Screen name={routes.STORE} component={StoreScreen} />
+        <BottomTab.Screen name={routes.CHECK} component={CheckView} />
+      </BottomTab.Navigator>
+    </QueryClientProvider>
   );
 }
