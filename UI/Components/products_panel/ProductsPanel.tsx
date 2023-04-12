26d692a8889c5a011f2f04f39fc08f22b1b11486
Daouadji Aymen <daouadji506@gmail.com>
2023-04-12 06:56:12 +0100

    💄 create ProductsPanel comp

--- UI/Components/products_panel/ProductsPanel.tsx
+++ UI/Components/products_panel/ProductsPanel.tsx
@@ -1,22 +1,50 @@
-import { ActivityIndicator, ScrollView, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  ScrollView,
+  View,
+} from 'react-native';
 import styles from './style/index';
 import CategoriesItem from '@components/categories_item';
 import { useQuery } from '@tanstack/react-query';
-import { fetchCategoriesNames } from '@api/firebase/firebase.utils';
+import {
+  fetchCategoriesNames,
+  fetchProductsByCategoryName,
+} from '@api/firebase/firebase.utils';
 import Colors from '@components/config/Colors';
 import { useState } from 'react';
+import ProductItem from '@components/product_item';
+import milk from '@toPng/milk_candia.png';
 
+const windowWidth = Dimensions.get('window').width;
 interface ProductsPanelProps {}
 export default function ProductsPanel({}: ProductsPanelProps) {
+  const [selectedIndex, setSelectedIndex] = useState(0);
   const {
     isLoading,
     error,
     data: categories,
+    isSuccess: categoriesIsSuccess,
   } = useQuery({
     queryKey: ['categoriesNames'],
     queryFn: fetchCategoriesNames,
   });
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  const selectedCategory = categoriesIsSuccess
+    ? categories[selectedIndex]
+    : undefined;
+  const {
+    isLoading: selectedCategoryIsLoading,
+    isFetching: selectedCategoryIsFetching,
+    error: selectedCategorieError,
+    data: categoryData,
+    isSuccess: selectedCategorieIsSuccess,
+  } = useQuery({
+    queryKey: ['products', selectedCategory],
+    queryFn: () => fetchProductsByCategoryName(selectedCategory),
+    enabled: !!selectedCategory,
+  });
+
   return (
     <View style={styles.ProductsPanel}>
       {isLoading ? (
@@ -29,23 +57,61 @@ export default function ProductsPanel({}: ProductsPanelProps) {
           <ActivityIndicator color={Colors.primary} size={50} />
         </View>
       ) : (
-        <ScrollView
-          pagingEnabled={false}
-          style={styles.pickerContainer}
-          horizontal={true}
-          centerContent={true}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={[styles.categoriesContainer]}
-        >
-          {categories?.map((category, index) => (
-            <CategoriesItem
-              name={category}
-              key={index}
-              selected={selectedIndex == index}
-              onPress={() => setSelectedIndex(index)}
-            />
-          ))}
-        </ScrollView>
+        <View style={styles.ProductsPanelContainer}>
+          <ScrollView
+            pagingEnabled={false}
+            style={styles.pickerContainer}
+            horizontal={true}
+            centerContent={true}
+            contentContainerStyle={[styles.categoriesContainer]}
+          >
+            {categories?.map((category, index) => (
+              <CategoriesItem
+                name={category}
+                key={index}
+                selected={selectedIndex == index}
+                onPress={() => setSelectedIndex(index)}
+              />
+            ))}
+          </ScrollView>
+          {selectedCategoryIsLoading || selectedCategoryIsFetching ? (
+            <View
+              style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <ActivityIndicator color={Colors.primary} size={50} />
+            </View>
+          ) : (
+            selectedCategorieIsSuccess && (
+              <FlatList
+                data={categoryData.products}
+                renderItem={({ item }) => {
+                  if (!item) return null;
+                  //TODO  fix the images url in firebase
+                  return (
+                    <ProductItem
+                      name={item.name}
+                      price={item.price}
+                      tag={item.tag}
+                      image={milk}
+                    />
+                  );
+                }}
+                keyExtractor={(item) => {
+                  if (!item) return null;
+                  return item.id;
+                }}
+                numColumns={2}
+                style={styles.productsListContainer}
+                contentContainerStyle={styles.productsList}
+                scrollEnabled={true}
+                columnWrapperStyle={styles.columnWrapper}
+              />
+            )
+          )}
+        </View>
       )}
     </View>
   );
--- UI/Components/products_panel/style/index.ts
+++ UI/Components/products_panel/style/index.ts
@@ -2,7 +2,9 @@ import { StyleSheet } from 'react-native';
 import Colors from '@colors';
 
 export default StyleSheet.create({
-  ProductsPanel: {},
+  ProductsPanel: {
+    flex: 1,
+  },
   pickerContainer: {
     flexGrow: 0,
   },
@@ -12,5 +14,27 @@ export default StyleSheet.create({
     gap: 5,
     alignItems: 'center',
     paddingHorizontal: 10,
+  },
+  ProductsPanelContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    flex: 1,
+  },
+  productsListContainer: {
+    alignSelf: 'center',
+    flex: 1,
+  },
+  productsList: {
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 10,
+    flexGrow: 1,
+    paddingBottom: 50,
+  },
+  columnWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 10,
   },
 });
