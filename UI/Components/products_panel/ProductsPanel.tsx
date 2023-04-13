e668537f36bdcef7f82e682dc23220573b3dee71
Daouadji Aymen <daouadji506@gmail.com>
2023-04-13 01:52:22 +0100

    ✨ add the ability to Refresh, fetchProductsByCategoryID

--- UI/Components/products_panel/ProductsPanel.tsx
+++ UI/Components/products_panel/ProductsPanel.tsx
@@ -2,6 +2,7 @@ import {
   ActivityIndicator,
   Dimensions,
   FlatList,
+  RefreshControl,
   ScrollView,
   View,
 } from 'react-native';
@@ -10,12 +11,13 @@ import CategoriesItem from '@components/categories_item';
 import { useQuery } from '@tanstack/react-query';
 import {
   fetchCategoriesNames,
-  fetchProductsByCategoryName,
+  fetchProductsByCategoryID,
 } from '@api/firebase/firebase.utils';
 import Colors from '@components/config/Colors';
 import { useState } from 'react';
 import ProductItem from '@components/product_item';
 import milk from '@toPng/milk_candia.png';
+import AppText from '@components/basic/app_text';
 
 const windowWidth = Dimensions.get('window').width;
 interface ProductsPanelProps {}
@@ -37,11 +39,15 @@ export default function ProductsPanel({}: ProductsPanelProps) {
     isLoading: selectedCategoryIsLoading,
     isFetching: selectedCategoryIsFetching,
     error: selectedCategorieError,
-    data: categoryData,
-    isSuccess: selectedCategorieIsSuccess,
+    data: products,
+    isSuccess: selectedCategoryIsSuccess,
+    refetch,
   } = useQuery({
-    queryKey: ['products', selectedCategory],
-    queryFn: () => fetchProductsByCategoryName(selectedCategory),
+    queryKey: ['products', selectedCategory?.id],
+    queryFn: () => {
+      if (selectedCategory)
+        return fetchProductsByCategoryID(selectedCategory.id);
+    },
     enabled: !!selectedCategory,
   });
 
@@ -65,10 +71,10 @@ export default function ProductsPanel({}: ProductsPanelProps) {
             centerContent={true}
             contentContainerStyle={[styles.categoriesContainer]}
           >
-            {categories?.map((category, index) => (
+            {categories?.map(({ id, name }, index) => (
               <CategoriesItem
-                name={category}
-                key={index}
+                name={name}
+                key={id}
                 selected={selectedIndex == index}
                 onPress={() => setSelectedIndex(index)}
               />
@@ -84,23 +90,23 @@ export default function ProductsPanel({}: ProductsPanelProps) {
               <ActivityIndicator color={Colors.primary} size={50} />
             </View>
           ) : (
-            selectedCategorieIsSuccess && (
+            selectedCategoryIsSuccess && (
+              // <AppText text="no data" />
               <FlatList
-                data={categoryData.products}
+                data={products}
                 renderItem={({ item }) => {
                   if (!item) return null;
                   //TODO  fix the images url in firebase
                   return (
                     <ProductItem
                       name={item.name}
-                      price={item.price}
-                      tag={item.tag}
+                      price={item.price.amount.toString()}
+                      tag={item.price.currency}
                       image={milk}
                     />
                   );
                 }}
                 keyExtractor={(item) => {
-                  if (!item) return null;
                   return item.id;
                 }}
                 numColumns={2}
@@ -108,6 +114,18 @@ export default function ProductsPanel({}: ProductsPanelProps) {
                 contentContainerStyle={styles.productsList}
                 scrollEnabled={true}
                 columnWrapperStyle={styles.columnWrapper}
+                showsHorizontalScrollIndicator={false}
+                refreshing={true}
+                refreshControl={
+                  <RefreshControl
+                    refreshing={isLoading}
+                    onRefresh={refetch}
+                    progressBackgroundColor={Colors.primary}
+                    colors={[Colors.white]}
+                  >
+                    <ActivityIndicator color={Colors.primary} size={50} />
+                  </RefreshControl>
+                }
               />
             )
           )}
