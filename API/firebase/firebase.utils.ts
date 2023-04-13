71b81fe1341bfb4e0c5de1e2f43707d2387b60c0
Daouadji Aymen <daouadji506@gmail.com>
2023-04-13 01:50:37 +0100

    ✨ implement firebase utility function  and add some fake data to push into firestore

--- API/firebase/firebase.utils.ts
+++ API/firebase/firebase.utils.ts
@@ -1,6 +1,7 @@
 import { getApp, getApps, initializeApp } from 'firebase/app';
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
 import {
+  addDoc,
   collection,
   doc,
   getDocs,
@@ -31,6 +32,20 @@ const auth = initializeAuth(firebaseApp, {
 });
 const db = getFirestore();
 
+export type Category = {
+  name: string;
+  id: string;
+};
+interface Product {
+  id: string;
+  name: string;
+  price: {
+    amount: number;
+    currency: string;
+  };
+  categoryID: string;
+}
+
 export const addUserOnSignUp = async (
   displayName: string,
   email: string,
@@ -60,64 +75,193 @@ export const addUserOnSignUp = async (
   }
 };
 
-// export const addCollectionToFirebase = async (
-//   collectionKey,
-//   categoriesArray,
-// ) => {
-//   const collectionRef = firestore.collection(collectionKey);
-//   const batch = firestore.batch();
-//   categoriesArray.forEach((obj) => {
-//     const newDocRef = collectionRef.doc();
-//     batch.set(newDocRef, obj);
-//   });
-//   return await batch.commit();
-// };
-
-// export const convertCollectionsSnapshotToMap = (
-//   snapshot: firebase.firestore.QuerySnapshot<firebase.firestore.DocumentData>,
-// ): Record<string, any> => {
-//   const transformatedCollections = snapshot.docs.map((doc) => {
-//     const { name, products } = doc.data();
-//     return {
-//       id: doc.id,
-//       name,
-//       products,
-//     };
-//   });
-//   return transformatedCollections.reduce(
-//     (acc: Record<string, any>, collection) => {
-//       acc[collection.name.toLowerCase()] = collection;
-//       return acc;
-//     },
-//     {},
-//   );
-// };
-
 export const fetchCategoriesNames = async () => {
-  const categoriesCollection = collection(db, 'CategoriesNames');
+  const categoriesCollection = collection(db, 'categories');
   const q = query(categoriesCollection);
   const querySnapshot = await getDocs(q);
 
   return querySnapshot.docs.map((document) => {
-    // console.log(document.data().name);
-    return document.data().name;
+    return document.data() as Category;
   });
 };
 
-export type Category = {
-  name: string;
-  products: any[];
+export const fetchProductsByCategoryID = async (categoryID: string) => {
+  const productsRef = collection(db, 'products');
+  const q = query(productsRef, where('categoryID', '==', categoryID));
+  const querySnapshot = await getDocs(q);
+  const products = querySnapshot.docs.map((document) => ({
+    id: document.id,
+    ...document.data(),
+  }));
+  return products as Product[];
 };
 
-export const fetchProductsByCategoryName = async (categoryName: string) => {
-  const categoriesRef = collection(db, 'Categories');
-  const categoryQuery = query(categoriesRef, where('name', '==', categoryName));
-  const querySnapshot = await getDocs(categoryQuery);
+export const fetchProductsByName = async (search: string) => {
+  const q = query(
+    collection(db, 'products'),
+    where('name', '>=', search),
+    where('name', '<=', search + '\uf8ff'),
+  );
+  const querySnapshot = await getDocs(q);
 
-  const categoryDoc = querySnapshot.docs[0];
-  // console.log(categoryDoc.data());
-  return categoryDoc.data() as Category;
+  const products = querySnapshot.docs.map((document) => {
+    return {
+      id: document.id,
+      ...document.data(),
+    } as Product;
+  });
+  return products;
 };
+
+export const addCategory = async (name: string) => {
+  const categoryDocRef = doc(collection(db, 'categories'));
+  await setDoc(categoryDocRef, { name, products: [] });
+  return categoryDocRef.id;
+};
+export const addProductToCategory = async (
+  categoryId: string,
+  product: Omit<Product, 'id'>,
+) => {
+  const productDocRef = doc(
+    collection(db, 'categories', categoryId, 'products'),
+  );
+  await setDoc(productDocRef, product);
+  return productDocRef.id;
+};
+
+const categories: Category[] = [
+  {
+    id: '1',
+    name: 'Drinks',
+  },
+
+  {
+    id: '2',
+    name: 'Dairy products',
+  },
+  {
+    id: '3',
+    name: 'Candies',
+  },
+];
+
+const products: Product[] = [
+  {
+    id: '1',
+    name: 'Milk',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '2',
+  },
+  {
+    id: '2',
+    name: 'cheese',
+    price: {
+      amount: 100,
+      currency: 'Dzd',
+    },
+    categoryID: '2',
+  },
+  {
+    id: '3',
+    name: 'Yogurt',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '2',
+  },
+  {
+    id: '4',
+    name: 'Butter',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '2',
+  },
+  {
+    id: '5',
+    name: 'jolly rancher candy',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '3',
+  },
+  {
+    id: '6',
+    name: 'milky-way',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '3',
+  },
+  {
+    id: '7',
+    name: 'nestle smarties candy',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '3',
+  },
+  {
+    id: '8',
+    name: 'jus',
+    price: {
+      amount: 1050,
+      currency: 'Dzd',
+    },
+    categoryID: '1',
+  },
+  {
+    id: '9',
+    name: 'water',
+    price: {
+      amount: 10,
+      currency: 'Dzd',
+    },
+    categoryID: '1',
+  },
+  {
+    id: '10',
+    name: 'tea',
+    price: {
+      amount: 1044,
+      currency: 'Dzd',
+    },
+    categoryID: '1',
+  },
+  {
+    id: '11',
+    name: 'Soft drinks',
+    price: {
+      amount: 1005,
+      currency: 'Dzd',
+    },
+    categoryID: '1',
+  },
+];
+
+export async function addCategories() {
+  const categoriesRef = collection(db, 'categories');
+
+  for (const category of categories) {
+    await addDoc(categoriesRef, category);
+  }
+}
+
+export async function addProducts() {
+  const productsRef = collection(db, 'products');
+
+  for (const product of products) {
+    await addDoc(productsRef, product);
+  }
+}
 
 export { auth, db };
 export default firebaseApp;
