310e3fff605b7cd6212e9ed1a570f42af53cc720
Daouadji Aymen <daouadji506@gmail.com>
2023-04-17 07:02:15 +0100

    ✅ update bagStore

--- services/zustand/bagStore.ts
+++ services/zustand/bagStore.ts
@@ -13,8 +13,8 @@ interface BagState {
   addItem: (item: BagItem) => void;
   removeItem: (id: string) => void;
   clearBag: () => void;
-  addQuantity: (id: string, quantity: number) => void;
-  removeQuantity: (id: string, quantity: number) => void;
+  addQuantity: (id: string) => void;
+  removeQuantity: (id: string) => void;
 }
 
 export const useBagStore = create<BagState>((set) => ({
@@ -26,20 +26,20 @@ export const useBagStore = create<BagState>((set) => ({
       bagItems: state.bagItems.filter((item) => item.id !== id),
     })),
   clearBag: () => set((state) => ({ bagItems: [] })),
-  addQuantity: (id: string, quantity: number) =>
+  addQuantity: (id: string) =>
     set((state) => ({
       bagItems: state.bagItems.map((item) => {
         if (item.id === id) {
-          return { ...item, quantity: item.quantity + quantity };
+          return { ...item, quantity: item.quantity + 1 };
         }
         return item;
       }),
     })),
-  removeQuantity: (id: string, quantity: number) =>
+  removeQuantity: (id: string) =>
     set((state) => ({
       bagItems: state.bagItems.map((item) => {
-        if (item.id === id) {
-          return { ...item, quantity: item.quantity - quantity };
+        if (item.id === id && item.quantity > 1) {
+          return { ...item, quantity: item.quantity - 1 };
         }
         return item;
       }),
