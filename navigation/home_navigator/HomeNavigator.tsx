be9c69207ff4458755b2c629eacdcc19295845b1
Daouadji Aymen <daouadji506@gmail.com>
2023-04-17 02:27:31 +0100

    ✨ use MyBagScreen

--- navigation/home_navigator/HomeNavigator.tsx
+++ navigation/home_navigator/HomeNavigator.tsx
@@ -1,24 +1,16 @@
-import { View } from 'react-native';
 import styles from './style/index';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { AntDesign, Feather } from '@expo/vector-icons';
-import Colors from '@components/config/Colors';
 import routes from '@navigation/routes';
 import TopNavigation from '@components/top_navigation';
 import StoreScreen from '@screens/store_screen';
 import BottomTabBar from '@components/bottom_tab_bar';
-import AppText from '@components/basic/app_text';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import MyBagScreen from '@screens/my_bag_screen';
 
 const BottomTab = createBottomTabNavigator();
 interface HomeNavigatorProps {}
 export default function HomeNavigator({}: HomeNavigatorProps) {
   const queryClient = new QueryClient();
-  const CheckView = () => (
-    <View>
-      <AppText text={'Check please'} />
-    </View>
-  );
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -29,7 +21,7 @@ export default function HomeNavigator({}: HomeNavigatorProps) {
         tabBar={(props) => <BottomTabBar props={props} />}
       >
         <BottomTab.Screen name={routes.STORE} component={StoreScreen} />
-        <BottomTab.Screen name={routes.CHECK} component={CheckView} />
+        <BottomTab.Screen name={routes.CHECK} component={MyBagScreen} />
       </BottomTab.Navigator>
     </QueryClientProvider>
   );
