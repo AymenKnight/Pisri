44f33ef9c3a6d69ee17a7759ec73e7096ae3daed
Daouadji Aymen <daouadji506@gmail.com>
2023-04-18 02:40:52 +0100

    ➕ add borderWidth prop

--- UI/Components/border_separator/BorderSeparator.tsx
+++ UI/Components/border_separator/BorderSeparator.tsx
@@ -4,11 +4,18 @@ import Colors from '@components/config/Colors';
 
 interface BorderSeparatorProps {
   borderColor?: string;
+  borderWidth?: number;
 }
 export default function BorderSeparator({
   borderColor = Colors.borders,
+  borderWidth = 1,
 }: BorderSeparatorProps) {
   return (
-    <View style={[styles.BorderSeparator, { borderColor: borderColor }]}></View>
+    <View
+      style={[
+        styles.BorderSeparator,
+        { borderColor: borderColor, borderWidth: borderWidth },
+      ]}
+    ></View>
   );
 }
--- UI/Components/border_separator/style/index.ts
+++ UI/Components/border_separator/style/index.ts
@@ -4,7 +4,6 @@ import Colors from '@colors';
 export default StyleSheet.create({
   BorderSeparator: {
     borderStyle: 'solid',
-    borderWidth: 1,
     alignSelf: 'stretch',
     borderRadius: 100,
   },
