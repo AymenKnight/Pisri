0673048889355cc98874102428dbb69213ba5a3d
Daouadji Aymen <daouadji506@gmail.com>
2023-04-18 02:43:27 +0100

    🎨 improve style

--- UI/screens/my_bag_screen/style/index.ts
+++ UI/screens/my_bag_screen/style/index.ts
@@ -1,14 +1,14 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 import Colors from '@colors';
 import font from '@components/config/font';
-
+const screenHeight = Dimensions.get('window').height;
 export default StyleSheet.create({
   MyBagScreen: {
     display: 'flex',
-    flex: 1,
     alignItems: 'stretch',
-    gap: 10,
+    gap: 20,
     paddingHorizontal: 20,
+    maxHeight: screenHeight * 0.7,
   },
   bigTitle: {
     fontFamily: font.PublicSans_800ExtraBold,
