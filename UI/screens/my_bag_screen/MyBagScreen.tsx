d85f22edeb8070342cae1de65295533f73932ed7
Daouadji Aymen <daouadji506@gmail.com>
2023-05-01 21:10:42 +0100

    ✨ navigate to checkout steps

--- UI/screens/my_bag_screen/MyBagScreen.tsx
+++ UI/screens/my_bag_screen/MyBagScreen.tsx
@@ -3,14 +3,31 @@ import styles from './style/index';
 import AppText from '@components/basic/app_text';
 import PrimaryButton from '@components/basic/buttons/primary_button';
 import BagPanel from '@components/bag_panel';
+import routes from '@navigation/routes';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { BottomTabParamList } from '@navigation/home_navigator/HomeNavigator';
+import { CompositeNavigationProp } from '@react-navigation/native';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { MainStackParamList } from '@navigation/main_navigator/MainNavigator';
 
-interface MyBagScreenProps {}
-export default function MyBagScreen({}: MyBagScreenProps) {
+type MyBagScreenNavigationProp = CompositeNavigationProp<
+  BottomTabNavigationProp<BottomTabParamList, 'MyBag'>,
+  StackNavigationProp<MainStackParamList>
+>;
+interface MyBagScreenProps {
+  navigation: MyBagScreenNavigationProp;
+}
+export default function MyBagScreen({ navigation }: MyBagScreenProps) {
   return (
     <View style={styles.MyBagScreen}>
       <AppText text="My Bag" style={styles.bigTitle} />
       <BagPanel />
-      <PrimaryButton text="Checkout" />
+      <PrimaryButton
+        text="Checkout"
+        onPress={() => {
+          navigation.navigate(routes.Check);
+        }}
+      />
     </View>
   );
 }
