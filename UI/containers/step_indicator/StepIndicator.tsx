e3d46d71c6a36ac66977ab1964623578ef1dc360
Daouadji Aymen <daouadji506@gmail.com>
2023-05-02 22:57:14 +0100

    ✨ improve StepIndicator

--- UI/containers/step_indicator/StepIndicator.tsx
+++ UI/containers/step_indicator/StepIndicator.tsx
@@ -1,32 +1,66 @@
-import { ScrollView, View } from 'react-native';
+import { ScrollView, View, Dimensions } from 'react-native';
 import styles from './style/index';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import AppText from '@components/basic/app_text';
+import AppButton from '@components/basic/buttons/text_button/TextButton';
+import * as Animatable from 'react-native-animatable';
 
+const screenWidth = Dimensions.get('window').width;
 const steps = [
   { title: 'Step 1', completed: true },
   { title: 'Step 2', completed: false },
   { title: 'Step 3', completed: false },
-  // add more steps here
 ];
 interface StepIndicatorProps {}
 export default function StepIndicator({}: StepIndicatorProps) {
   const [currentStep, setCurrentStep] = useState(0);
+  const [currentX, setCurrentX] = useState(0);
+  const scrollViewRef = useRef<ScrollView>(null);
 
   return (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {steps.map((step, index) => (
-        <View
-          key={index}
-          style={[
-            styles.step,
-            index === currentStep && styles.currentStep,
-            step.completed && styles.completedStep,
-          ]}
-        >
-          <AppText text={step.title} />
-        </View>
-      ))}
-    </ScrollView>
+    <View style={styles.StepIndicator}>
+      <View style={styles.stepDotsContainer}>
+        {steps.map((step, index) => (
+          <Animatable.View
+            transition={['width', 'height', 'backgroundColor']}
+            key={index}
+            style={[styles.stepDot, index <= currentStep && styles.currentDot]}
+          />
+        ))}
+      </View>
+      <ScrollView
+        ref={scrollViewRef}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        scrollEnabled={false}
+        pagingEnabled={true}
+      >
+        {steps.map((step, index) => (
+          <View
+            key={index}
+            style={[
+              styles.step,
+              index === currentStep && styles.currentStep,
+              step.completed && styles.completedStep,
+            ]}
+          >
+            <AppText text={step.title} />
+          </View>
+        ))}
+      </ScrollView>
+      <AppButton
+        text="Next"
+        onPress={() => {
+          if (currentStep !== steps.length - 1) {
+            setCurrentStep(currentStep + 1);
+            setCurrentX(currentX + screenWidth);
+            scrollViewRef.current?.scrollTo({
+              x: currentX + screenWidth,
+              animated: true,
+            });
+          }
+        }}
+      />
+    </View>
   );
 }
--- UI/containers/step_indicator/style/index.ts
+++ UI/containers/step_indicator/style/index.ts
@@ -1,14 +1,40 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import Colors from '@colors';
-
+const screenWidth = Dimensions.get('window').width;
 export default StyleSheet.create({
-  StepIndicator: {},
-  step: {
+  StepIndicator: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    gap: 20,
+  },
+  step: {
     backgroundColor: '#ccc',
+    width: screenWidth,
+    paddingTop: 30,
   },
   currentStep: { backgroundColor: '#007AFF' },
   completedStep: { backgroundColor: '#2ECC71' },
+  stepDotsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    gap: 20,
+    paddingHorizontal: 15,
+  },
+  stepDot: {
+    height: 10,
+    width: 10,
+    borderRadius: 100,
+    backgroundColor: Colors.borders,
+  },
+  currentDot: {
+    height: 15,
+    width: 15,
+    borderRadius: 100,
+    backgroundColor: Colors.primary,
+  },
 });
