e368eaa53f8e8de8ecc009ac61b0f64f37db271a
Daouadji Aymen <daouadji506@gmail.com>
2023-05-02 22:59:06 +0100

    ⚰️ Remove dead code

--- Components/Basic/inputs/number_input_reducer/index.ts
+++
@@ -1,2 +0,0 @@
-import NumberInputReducer from './NumberInputReducer';
-export default NumberInputReducer;
--- Components/Basic/inputs/unit_value_reducer/index.ts
+++
@@ -1,2 +0,0 @@
-import UnitValueReducer from './UnitValueReducer';
-export default UnitValueReducer;
