5b251bf51db9b74e3054ab0319f07dd2c8648f93
Daouadji Aymen <daouadji506@gmail.com>
2023-05-04 18:50:18 +0100

    💄 DeliveryOptionsStep UI is finished

--- UI/Components/delivery_options_step/DeliveryOptionsStep.tsx
+++ UI/Components/delivery_options_step/DeliveryOptionsStep.tsx
@@ -2,14 +2,41 @@ import { View } from 'react-native';
 import styles from './style/index';
 import AppText from '@components/basic/app_text';
 import PrimaryButton from '@components/basic/buttons/primary_button';
-import Colors from '@components/config/Colors';
 import { MaterialIcons } from '@expo/vector-icons';
+import OptionItem from '@components/option_item';
+import BorderContainer from '@containers/border_container';
+import { useStepProgressStore } from '@stores/stepProgressStore';
 
 interface DeliveryOptionsStepProps {}
 export default function DeliveryOptionsStep({}: DeliveryOptionsStepProps) {
+  const { setIsScroll } = useStepProgressStore();
   return (
     <View style={styles.DeliveryOptionsStep}>
       <AppText text="Step 1: Delivery options" style={styles.stepName} />
+      <BorderContainer>
+        <OptionItem
+          title="The offer stay alive for:"
+          description="how long your offer stay on waiting list"
+          value={3}
+        />
+        <OptionItem
+          title="Deliver time:"
+          description="maximum time to deliver the product"
+          value={3}
+        />
+        <OptionItem
+          title="Distance:"
+          description="how far your order can be announced"
+          value={200}
+          tag="M"
+        />
+        <OptionItem
+          title="Tip:"
+          description="you have to pay the delivery guy right?"
+          value={3}
+          tag="Dzd"
+        />
+      </BorderContainer>
       <View style={styles.footerContainer}>
         <PrimaryButton
           text="Next"
@@ -17,6 +44,9 @@ export default function DeliveryOptionsStep({}: DeliveryOptionsStepProps) {
           //   <MaterialIcons name="location-on" size={20} color={Colors.white} />
           // }
           style={{ width: '80%', alignSelf: 'center' }}
+          onPress={() => {
+            setIsScroll();
+          }}
         />
         <AppText
           text="Please enable Gps and press on verify"
