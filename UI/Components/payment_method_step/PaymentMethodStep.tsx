7636826ff97318c7a7c7f011d3ba823a5d43cb1d
Daouadji Aymen <daouadji506@gmail.com>
2023-05-04 22:15:27 +0100

    ✨ Scroll to next Step

--- UI/Components/payment_method_step/PaymentMethodStep.tsx
+++ UI/Components/payment_method_step/PaymentMethodStep.tsx
@@ -2,17 +2,31 @@ import { View } from 'react-native';
 import styles from './style/index';
 import AppText from '@components/basic/app_text';
 import PrimaryButton from '@components/basic/buttons/primary_button';
-import { MaterialIcons } from '@expo/vector-icons';
-import Colors from '@components/config/Colors';
+import { useStepProgressStore } from '@stores/stepProgressStore';
+import BorderContainer from '@containers/border_container';
+import PaymentItem from '@components/payment_item';
 
 interface PaymentMethodStepProps {}
 export default function PaymentMethodStep({}: PaymentMethodStepProps) {
+  const { setIsScroll } = useStepProgressStore();
   return (
     <View style={styles.PaymentMethodStep}>
       <AppText text="Step 2: Payment method" style={styles.stepName} />
+      <BorderContainer>
+        <AppText
+          text="Please select a payment method"
+          style={styles.hintStyle}
+        />
+        <PaymentItem name="PayPal/CreditCard" />
+        <PaymentItem name="Mobile Pay" />
+        <PaymentItem name="Pay on arrive" />
+      </BorderContainer>
       <PrimaryButton
         text="Next"
         style={{ width: '80%', alignSelf: 'center' }}
+        onPress={() => {
+          setIsScroll();
+        }}
       />
     </View>
   );
