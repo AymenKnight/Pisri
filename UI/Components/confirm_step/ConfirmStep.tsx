03e0395b746d9d6527e7ef1e128b3ac280b7f8d1
Daouadji Aymen <daouadji506@gmail.com>
2023-05-05 22:51:04 +0100

    ✅ update ConfirmStep comp

--- UI/Components/confirm_step/ConfirmStep.tsx
+++ UI/Components/confirm_step/ConfirmStep.tsx
@@ -1,12 +1,45 @@
 import { View } from 'react-native';
 import styles from './style/index';
 import AppText from '@components/basic/app_text';
+import BorderContainer from '@containers/border_container';
+import AppButton from '@components/basic/buttons/text_button/TextButton';
+import PrimaryButton from '@components/basic/buttons/primary_button';
+import AppTextInput from '@components/basic/inputs/app_text_input';
 
 interface ConfirmStepProps {}
 export default function ConfirmStep({}: ConfirmStepProps) {
   return (
     <View style={styles.ConfirmStep}>
       <AppText text="Step 3: Confirm and publish!" style={styles.stepName} />
+      <BorderContainer style={styles.infoWrapper}>
+        <View style={styles.infoContainer}>
+          <AppText text="you are going to pay" style={styles.description} />
+          <View style={styles.priceContainer}>
+            <AppText text="900" style={styles.price} />
+            <AppText text="Dzd" style={styles.tag} />
+          </View>
+          <AppText text="using the option bellow:" style={styles.description} />
+        </View>
+        <BorderContainer style={styles.payTextContainer}>
+          <AppText text="Pay on arrive" style={styles.payText} />
+        </BorderContainer>
+      </BorderContainer>
+      <AppTextInput
+        label="Add a note for the delivery guy:"
+        inputProps={{
+          // value: costumeDetail,
+          // onChangeText: setCostumeDetail,
+          multiline: true,
+        }}
+      />
+      <View style={styles.controlsContainer}>
+        <PrimaryButton
+          text="Confirm"
+          style={styles.confirmButton}
+          onPress={() => {}}
+        />
+        <PrimaryButton text="Cancel" style={styles.cancelButton} />
+      </View>
     </View>
   );
 }
--- UI/Components/confirm_step/style/index.ts
+++ UI/Components/confirm_step/style/index.ts
@@ -3,10 +3,72 @@ import Colors from '@colors';
 import font from '@components/config/font';
 
 export default StyleSheet.create({
-  ConfirmStep: { flex: 1, display: 'flex', flexDirection: 'column' },
+  ConfirmStep: {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    gap: 20,
+  },
   stepName: {
     fontFamily: font.PublicSans_800ExtraBold,
     fontSize: 20,
     color: Colors.fontColor,
+  },
+  infoWrapper: {
+    gap: 15,
+  },
+  infoContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignSelf: 'stretch',
+    gap: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  priceContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 2,
+  },
+  price: {
+    color: Colors.goodGreen,
+    fontFamily: font.PublicSans_800ExtraBold,
+    fontSize: 16,
+  },
+  tag: {
+    alignSelf: 'flex-end',
+    color: Colors.primary,
+    fontFamily: font.PublicSans_800ExtraBold,
+    fontSize: 14,
+  },
+  description: {
+    fontFamily: font.PublicSans_600SemiBold,
+    fontSize: 16,
+    color: Colors.fontColor,
+  },
+  payTextContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  payText: {
+    color: Colors.primary,
+    fontFamily: font.PublicSans_600SemiBold,
+    fontSize: 18,
+  },
+  controlsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    gap: 10,
+  },
+  confirmButton: {
+    backgroundColor: Colors.goodGreen,
+  },
+  cancelButton: {
+    backgroundColor: Colors.hot_red,
   },
 });
