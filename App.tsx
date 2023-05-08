596e073abc8b2548385c5c4185fa9595b487eaec
Daouadji Aymen <daouadji506@gmail.com>
2023-05-08 16:03:51 +0100

    🎨 add a theme for  the NavigationContainer

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import Colors from '@components/config/Colors';
 import Overlay from '@containers/modals/overlay';
 import AuthNavigator from '@navigation/auth_navigator';
 import { NavigationContainer } from '@react-navigation/native';
@@ -5,17 +6,17 @@ import { NavigationContainer } from '@react-navigation/native';
 export default function App() {
   return (
     <NavigationContainer
-    // theme={{
-    //   dark: false,
-    //   colors: {
-    //     background: signInVisible ? Colors.black : Colors.white,
-    //     border: Colors.borders,
-    //     text: Colors.black,
-    //     primary: Colors.primary,
-    //     notification: Colors.coldBlue,
-    //     card: Colors.white,
-    //   },
-    // }}
+      theme={{
+        dark: false,
+        colors: {
+          background: Colors.white,
+          border: Colors.borders,
+          text: Colors.black,
+          primary: Colors.primary,
+          notification: Colors.coldBlue,
+          card: Colors.white,
+        },
+      }}
     >
       <AuthNavigator />
       <Overlay />
