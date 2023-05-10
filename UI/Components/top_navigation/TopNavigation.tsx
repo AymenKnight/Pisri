8c265f69a3a79d7892dee537764830aa7c884841
Daouadji Aymen <daouadji506@gmail.com>
2023-05-10 21:51:23 +0100

    ✨ navigate to ACTIVITY screen

--- UI/Components/top_navigation/TopNavigation.tsx
+++ UI/Components/top_navigation/TopNavigation.tsx
@@ -4,11 +4,25 @@ import WelcomeLogo from '@toSvg/welcome_logo.svg';
 import AppButton from '@components/basic/buttons/text_button/TextButton';
 import { Entypo, MaterialIcons } from '@expo/vector-icons';
 import { auth } from '@api/firebase/firebase.utils';
+import {
+  CompositeNavigationProp,
+  useNavigation,
+} from '@react-navigation/native';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { MainStackParamList } from '@navigation/main_navigator/MainNavigator';
+import routes from '@navigation/routes';
+import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { BottomTabParamList } from '@navigation/home_navigator/HomeNavigator';
 
+type NavigationProp = CompositeNavigationProp<
+  StackNavigationProp<MainStackParamList>,
+  BottomTabNavigationProp<BottomTabParamList>
+>;
 interface TopNavigationProps {
   onBack?: () => void;
 }
 export default function TopNavigation({ onBack }: TopNavigationProps) {
+  const navigation = useNavigation<NavigationProp>();
   return (
     <View style={styles.TopNavigation}>
       {onBack ? (
@@ -22,7 +36,9 @@ export default function TopNavigation({ onBack }: TopNavigationProps) {
         <AppButton
           padding={0}
           icon={<Entypo name="menu" size={40} color="black" />}
-          onPress={() => {}}
+          onPress={() => {
+            navigation.navigate(routes.ACTIVITY);
+          }}
         />
       )}
       <WelcomeLogo width={120} />
