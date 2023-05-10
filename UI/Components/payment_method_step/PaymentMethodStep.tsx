76fd446e7d972eb4a135b5e821ad43085af8e150
Daouadji Aymen <daouadji506@gmail.com>
2023-05-10 18:14:05 +0100

    ✨ navigate to Confirmation Step and enable scrolling

--- UI/Components/payment_method_step/PaymentMethodStep.tsx
+++ UI/Components/payment_method_step/PaymentMethodStep.tsx
@@ -1,16 +1,22 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import styles from './style/index';
 import AppText from '@components/basic/app_text';
 import PrimaryButton from '@components/basic/buttons/primary_button';
 import { useStepProgressStore } from '@stores/stepProgressStore';
 import BorderContainer from '@containers/border_container';
 import PaymentItem from '@components/payment_item';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { StepStackParamList } from '@navigation/step_navigator/StepNavigator';
+import { useNavigation } from '@react-navigation/native';
+import routes from '@navigation/routes';
 
+type NavigationProp = StackNavigationProp<StepStackParamList>;
 interface PaymentMethodStepProps {}
 export default function PaymentMethodStep({}: PaymentMethodStepProps) {
-  const { setIsScroll } = useStepProgressStore();
+  const navigation = useNavigation<NavigationProp>();
+
   return (
-    <View style={styles.PaymentMethodStep}>
+    <ScrollView contentContainerStyle={styles.PaymentMethodStep}>
       <AppText text="Step 2: Payment method" style={styles.stepName} />
       <BorderContainer style={styles.optionsContainer}>
         <AppText
@@ -28,9 +34,9 @@ export default function PaymentMethodStep({}: PaymentMethodStepProps) {
         text="Next"
         style={{ width: '80%', alignSelf: 'center' }}
         onPress={() => {
-          setIsScroll();
+          navigation.navigate(routes.ConfirmationStep);
         }}
       />
-    </View>
+    </ScrollView>
   );
 }
