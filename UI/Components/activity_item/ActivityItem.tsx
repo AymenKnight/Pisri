7e58f7ee131c6f0ae933f51b2248c9fe0bee11c9
Daouadji Aymen <daouadji506@gmail.com>
2023-05-10 21:54:05 +0100

    ✅ Update ActivityItem : add icon for each status , create sliding menu bar and handle some state logic

--- UI/Components/activity_item/ActivityItem.tsx
+++ UI/Components/activity_item/ActivityItem.tsx
@@ -1,23 +1,43 @@
 import { View } from 'react-native';
 import styles from './style/index';
 import AppText from '@components/basic/app_text';
-import { Feather, MaterialCommunityIcons } from '@expo/vector-icons';
+import {
+  Entypo,
+  Feather,
+  FontAwesome,
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+} from '@expo/vector-icons';
 import Colors from '@components/config/Colors';
 import AppButton from '@components/basic/buttons/text_button/TextButton';
+import * as Animatable from 'react-native-animatable';
+import { useState } from 'react';
 
 type DeliveryStatus =
   | 'delivering'
   | 'Finished'
-  | 'cancelled'
   | 'pending'
-  | 'deliveried';
+  | 'onDoor'
+  | 'notified';
 
 interface ActivityItemProps {
-  Status?: DeliveryStatus;
+  status?: DeliveryStatus;
 }
-export default function ActivityItem({ Status }: ActivityItemProps) {
+export default function ActivityItem({ status }: ActivityItemProps) {
+  //TODO provide real data (props)
+  //TODO close menuBar when clicked outside or by time
+  //TODO show remaining time in some status
+  //TODO refactor menuBar to single comp
+  //TODO handle the logic (onPress and state)
+  //TODO handle time format
+  const [menuIsShown, showMenu] = useState(false);
   return (
-    <View style={styles.ActivityItem}>
+    <View
+      style={[
+        styles.ActivityItem,
+        { borderBottomRightRadius: menuIsShown ? 0 : 20 },
+      ]}
+    >
       <View style={styles.headerContainer}>
         <AppText text={`Receipt #1235467`} style={styles.ReceiptText} />
         <View style={styles.StatusContainer}>
@@ -36,24 +56,154 @@ export default function ActivityItem({ Status }: ActivityItemProps) {
           <AppText text={'900'} style={styles.price} />
           <AppText text={'Dzd'} style={styles.tag} />
         </View>
-        <AppButton
-          style={styles.menuButton}
-          padding={0}
-          icon={
-            <MaterialCommunityIcons
-              name="dots-horizontal"
-              size={25}
-              color={Colors.borders}
+        {status == 'onDoor' ? (
+          <Animatable.View
+            style={styles.hereButtonContainer}
+            animation="swing"
+            duration={1200}
+            useNativeDriver={true}
+            iterationDelay={5000}
+            iterationCount="infinite"
+          >
+            <AppButton
+              style={styles.hereButton}
+              textStyle={styles.hereButtonText}
+              padding={8}
+              text="It's here!"
             />
-          }
-        />
+          </Animatable.View>
+        ) : menuIsShown ? (
+          <Animatable.View
+            animation="jello"
+            duration={1000}
+            style={styles.popMenu}
+            // onPress={hundleMenuPress}
+          >
+            <AppButton
+              style={{
+                backgroundColor: Colors.goodGreen,
+                borderRadius: 100,
+                width: 30,
+                height: 30,
+              }}
+              padding={0}
+              icon={
+                <MaterialCommunityIcons
+                  name="account-group"
+                  size={15}
+                  color="white"
+                />
+              }
+            />
+            <AppButton
+              style={{
+                backgroundColor: Colors.coldBlue,
+                borderRadius: 100,
+                width: 30,
+                height: 30,
+              }}
+              padding={0}
+              icon={<FontAwesome name="pause" size={12} color="white" />}
+            />
+            <AppButton
+              style={{
+                backgroundColor: Colors.Unfocused_Blue,
+                borderRadius: 100,
+                width: 30,
+                height: 30,
+              }}
+              padding={0}
+              icon={
+                <FontAwesome
+                  name="pencil-square-o"
+                  size={15}
+                  color={Colors.white}
+                />
+              }
+            />
+            <AppButton
+              style={{
+                backgroundColor: Colors.hot_red,
+                borderRadius: 100,
+                width: 30,
+                height: 30,
+              }}
+              padding={0}
+              icon={<Feather name="x" size={18} color="white" />}
+            />
+          </Animatable.View>
+        ) : (
+          <AppButton
+            style={styles.menuButton}
+            padding={0}
+            onPress={() => showMenu(true)}
+            icon={
+              <MaterialCommunityIcons
+                name="dots-horizontal"
+                size={25}
+                color={Colors.borders}
+              />
+            }
+          />
+        )}
       </View>
-      <MaterialCommunityIcons
-        name="progress-clock"
-        size={20}
-        color="white"
-        style={styles.StatusIcon}
-      />
+      <View
+        style={[
+          styles.StatusIconContainer,
+          {
+            backgroundColor:
+              status == 'delivering'
+                ? Colors.primary
+                : status == 'notified'
+                ? Colors.goodGreen
+                : status == 'pending'
+                ? Colors.coldBlue
+                : status == 'Finished'
+                ? Colors.goodGreen
+                : status == 'onDoor'
+                ? Colors.coldBlue
+                : Colors.primary,
+          },
+        ]}
+      >
+        {status == 'delivering' ? (
+          <MaterialCommunityIcons
+            name="progress-clock"
+            size={18}
+            color="white"
+            style={styles.StatusIcon}
+          />
+        ) : status == 'notified' ? (
+          <MaterialCommunityIcons
+            name="account-group"
+            size={15}
+            color="white"
+            style={styles.StatusIcon}
+          />
+        ) : status == 'pending' ? (
+          <MaterialCommunityIcons
+            name="walk"
+            size={18}
+            color="white"
+            style={styles.StatusIcon}
+          />
+        ) : status == 'Finished' ? (
+          <Entypo
+            name="check"
+            size={18}
+            color={Colors.white}
+            style={styles.StatusIcon}
+          />
+        ) : status == 'onDoor' ? (
+          <MaterialCommunityIcons
+            name="bell-alert"
+            size={15}
+            color={Colors.white}
+          />
+        ) : (
+          <MaterialCommunityIcons name="door" size={18} color={Colors.white} />
+        )}
+      </View>
       <View style={styles.timerContainer}>
         <AppText text={'remaining:'} style={styles.timerText} />
         <AppText text={'20:25'} style={styles.time} />
