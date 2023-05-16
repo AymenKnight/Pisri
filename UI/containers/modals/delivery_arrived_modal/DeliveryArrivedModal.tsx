578941e14cccfda8dd0accb7102229c590f494d4
Daouadji Aymen <daouadji506@gmail.com>
2023-05-16 16:37:12 +0100

    💄 Create DeliveryArrivedModal comp

--- UI/containers/modals/delivery_arrived_modal/DeliveryArrivedModal.tsx
+++ UI/containers/modals/delivery_arrived_modal/DeliveryArrivedModal.tsx
@@ -1,7 +1,57 @@
 import { View } from 'react-native';
 import styles from './style/index';
-
-interface DeliveryArrivedModalProps {}
-export default function DeliveryArrivedModal({}: DeliveryArrivedModalProps) {
-  return <View style={styles.DeliveryArrivedModal}></View>;
+import AppText from '@components/basic/app_text';
+import BorderContainer from '@containers/border_container';
+import { Ionicons } from '@expo/vector-icons';
+import Colors from '@components/config/Colors';
+import * as Animatable from 'react-native-animatable';
+interface DeliveryArrivedModalProps {
+  receiptId: string;
+  verificationCode: string;
+}
+export default function DeliveryArrivedModal({
+  receiptId,
+  verificationCode,
+}: DeliveryArrivedModalProps) {
+  return (
+    <View style={styles.DeliveryArrivedModal}>
+      <View style={styles.headerContainer}>
+        <AppText text="Delivery requests" style={styles.HeaderTextStyle} />
+        <AppText
+          text={`Receipt #${receiptId}`}
+          style={styles.ReceiptTextStyle}
+        />
+      </View>
+      <BorderContainer style={styles.codeWrapper}>
+        <AppText
+          text="Please provide your delivery guy with the code bellow:"
+          style={styles.description}
+        />
+        <BorderContainer style={styles.codeContainer}>
+          <AppText text={verificationCode} style={styles.codeTextStyle} />
+        </BorderContainer>
+      </BorderContainer>
+      <View style={styles.controlsContainer}>
+        <AppText text="Not verified" style={styles.notVerifiedTextStyle} />
+        {/* <View style={styles.verifiedIconWrapper}>
+          <Animatable.View
+            // iterationCount="infinite"
+            // animation={{
+            //   from: {
+            //     transform: [{ rotate: '0deg' }],
+            //   },
+            //   to: {
+            //     transform: [{ rotate: '360deg' }],
+            //   },
+            // }}
+            // duration={1000}
+            // easing={'linear'}
+            style={styles.verifiedIconStyle}
+          >
+            <Ionicons name="reload" size={35} color="white" />
+          </Animatable.View>
+        </View> */}
+      </View>
+    </View>
+  );
 }
--- UI/containers/modals/delivery_arrived_modal/style/index.ts
+++ UI/containers/modals/delivery_arrived_modal/style/index.ts
@@ -1,6 +1,76 @@
 import { StyleSheet } from 'react-native';
 import Colors from '@colors';
+import font from '@components/config/font';
 
 export default StyleSheet.create({
-  DeliveryArrivedModal: {},
+  DeliveryArrivedModal: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 20,
+    paddingBottom: 20,
+  },
+  headerContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    flexDirection: 'column',
+    gap: 5,
+  },
+  HeaderTextStyle: {
+    fontFamily: font.PublicSans_900Black,
+    color: Colors.fontColor,
+    fontSize: 20,
+  },
+  ReceiptTextStyle: {
+    fontFamily: font.PublicSans_700Bold,
+    color: Colors.fontColor,
+    fontSize: 16,
+  },
+  description: {
+    fontFamily: font.PublicSans_700Bold,
+    color: Colors.fontColor,
+    fontSize: 15,
+    textAlign: 'center',
+  },
+  codeWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 20,
+  },
+  codeContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  codeTextStyle: {
+    fontFamily: font.PublicSans_700Bold,
+    color: Colors.primary,
+    fontSize: 15,
+  },
+  controlsContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: 10,
+  },
+  notVerifiedTextStyle: {
+    fontFamily: font.PublicSans_700Bold,
+    color: Colors.Unfocused_Blue,
+    fontSize: 18,
+  },
+  verifiedIconWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: Colors.coldBlue,
+    borderRadius: 100,
+    padding: 5,
+    width: 50,
+    height: 50,
+  },
+  verifiedIconStyle: {},
 });
