8b0a46a9989bbeff642d404ca132bc55dc9f7b1a
Daouadji Aymen <daouadji506@gmail.com>
2023-05-17 19:01:38 +0100

    ✅ Update MainNavigator

--- navigation/main_navigator/MainNavigator.tsx
+++ navigation/main_navigator/MainNavigator.tsx
@@ -3,21 +3,28 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import routes from '@navigation/routes';
 import HomeNavigator from '@navigation/home_navigator';
 import TopNavigation from '@components/top_navigation';
-import DeliveryFinished from '@screens/delivery_finished';
 import StepNavigator from '@navigation/step_navigator';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export type MainStackParamList = {
   HomeTabs: undefined;
   Check: undefined;
 };
-const MainStack = createNativeStackNavigator<MainStackParamList>();
+const MainStack = createStackNavigator<MainStackParamList>();
 interface MainNavigatorProps {}
 export default function MainNavigator({}: MainNavigatorProps) {
-  //TODO refactor DeliveryFinished to modal instead
   return (
     <MainStack.Navigator
       screenOptions={{
-        header: (props) => <TopNavigation />,
+        header: ({ navigation, options, route, back }) => {
+          return (
+            <TopNavigation
+            // onBack={
+            //   route.name === routes.Check ? navigation.goBack : undefined
+            // }
+            />
+          );
+        },
       }}
     >
       <MainStack.Screen name={routes.HomeTabs} component={HomeNavigator} />
